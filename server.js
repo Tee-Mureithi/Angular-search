19108c68db6cc9f3c9c5d5e4f2af242141af1177
Tee-Mureithi <esther.mureithi@student.moringaschool>
2022-02-12 01:32:19 +0300

    changed line in package.json to fix heroku deploy

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const express = require('express');
+const compression = require('compression');
 const path = require('path');
+const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/dist/<angular-tour-of-heroes>'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
-app.listen(process.env.PORT || 4200);
+const port = process.env.PORT || 8080;
+// Gzip
+app.use(compression());
+// Serve static files from the dist directory
+app.use(express.static(__dirname + '/../dist'));
+// Start the app by listening on the default Heroku port
+app.listen(port);
+// Return index.html for all GET requests for PathLocationStrategy
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../dist/index.html'));
+});
+console.log(`Server listening on ${port}`);
