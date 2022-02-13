e0a040fc700014846cca6b1694704f3e96154ba0
Tee-Mureithi <esther.mureithi@student.moringaschool>
2022-02-13 15:32:26 +0300

    added user interface to display results from the API

--- src/app/components/github-profile-card/github-profile-card.component.ts
+++ src/app/components/github-profile-card/github-profile-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-github-profile-card',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileCardComponent implements OnInit {
 
+  @Input() githubProfile:any;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/github-profile-data/github-profile-data.component.ts
+++ src/app/components/github-profile-data/github-profile-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-github-profile-data',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileDataComponent implements OnInit {
 
+  @Input() githubProfile:any;
   constructor() { }
 
   ngOnInit(): void {
