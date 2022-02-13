f1194399b52aab9fb82ffd972acdbb7b788fd74a
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-02-13 23:59:23 +0300

    environment ts location

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,10 @@ export const environment = {
   production: false
 };
 
+
+export const CLIENT_ID="Iv1.736a972e43d4ad8c";
+export const CLIENT_SECRET="731e9a6ea31208f48e71d3b4df1088b0b53d02ed";
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
