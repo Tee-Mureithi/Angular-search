5c5b2cb00d1d65be9ee8d1c3c0f00af368a7e0c5
Tee-Mureithi <esther.mureithi@student.moringaschool>
2022-02-13 03:00:43 +0300

    logic to fetch data from the server

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { CLIENT_ID, CLIENT_SECRET } from '../credentials/GithubCred';
 import { refsToArray } from '@angular/compiler/src/render3/util';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
     providedIn:'root'
@@ -11,13 +12,35 @@ export class GithubService{
     //For the Github Profile 
     public getProfile(SearchQuery: any){
         let dataURL = `https://api.github.com/users/${SearchQuery}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
-        return this.HttpClient.get(dataURL).pipe(
-           
+        return this.HttpClient.get<any>(dataURL).pipe(
+           catchError(this.handleErrors)
         );
     }
 
 
     //For the Github Repositories
+    public getRepos(SearchQuery: any[]){
+        let dataURL = `https://api.github.com/users/${SearchQuery}/repos?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+        return this.HttpClient.get<any[]>(dataURL).pipe(
+           catchError(this.handleErrors)
+        );
+    }
+
+
+     public handleErrors(error:HttpErrorResponse){
+         let errorMessage:string;
+         if(error.error instanceof ErrorEvent){
+             //Clearly a client side error
+             errorMessage = `MESSAGE:${error.error.message}`
+         }
+        else{
+            //This is a Server side error
+            errorMessage = `STATUS:${error.status} MESSAGE:${error.message}`
+        }
+        return throwError(errorMessage);
+     }
+
+   
 
 
 
