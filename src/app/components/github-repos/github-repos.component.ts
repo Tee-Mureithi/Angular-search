c4f2de0572ecaa6fb21fcb0e7204c418c8b3be0c
Tee-Mureithi <esther.mureithi@student.moringaschool>
2022-02-13 16:21:18 +0300

    Fetched and displayed repositories to DOM

--- src/app/components/github-repos/github-repos.component.ts
+++ src/app/components/github-repos/github-repos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-github-repos',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubReposComponent implements OnInit {
 
+  @Input()
+  githubRepos!: any[];
   constructor() { }
 
   ngOnInit(): void {
